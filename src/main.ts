ade8e42654c8e459dbc8621ff7ac3803d95f0d86
Bruno Niello <brunoniello@gmail.com>
2025-10-14 22:12:42 -0300

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 
 async function main() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("Conexa API")
+    .setDescription("The Conexa Star Wars API description")
+    .setVersion("1.0")
+    .addTag("conexa")
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 main();
